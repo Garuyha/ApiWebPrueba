347e3eb4a35c17eef08ecd7b97b23b8817349508
Ezequiel Alejandro Garcia <112414829+Garuyha@users.noreply.github.com>
2025-07-31 20:11:26 -0300

    node practica
    carpeta para repasar node.js

---
+++ node-practica/index.js
@@ -0,0 +1,11 @@
+let username = "garcis";
+let age = 30;
+let band = true;
+let points = [10, 20, 30]
+let user = {
+    name: "cian",
+    lastname: "sur"
+};
+
+console.log(user);
+//para ejecutar en la terminar escribir node "nombre del archivo" (index.js)
